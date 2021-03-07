fe6aed6f9bb50dfbfabae2f7cbb9e0e4724eeeab
nousi <nousidayo@gmail.com>
2021-03-07 11:12:33 +0900

    UserTest.phpにuse App\User;を追加
    Error: Class "Tests\Feature\User" not found
    が発生していたために追加

--- backend/tests/Feature/UserTest.php
+++ backend/tests/Feature/UserTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\User;
 
 class UserTest extends TestCase
 {
